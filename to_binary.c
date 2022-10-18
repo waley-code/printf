3f05697af93d5fe3010cafc399a74a7f89299c8e
waley-code <oyewaleadeosun@yahoo.com>
2022-10-18 07:07:05 +0100

    update task-2

--- to_binary.c
+++ to_binary.c
@@ -7,7 +7,7 @@ unsigned to_binary(unsigned int n)
     //counter for binary array
     int i = 0;
     while (n > 0) {
-        // storing remainder in binary array
+        //storing remainder in binary array
         binaryNum[i] = n % 2;
         n = n / 2;
         i++;
